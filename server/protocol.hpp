8ebf16812d8fa40e9ebc64195b8b0eaa6f600153
YoungJun Jeong <dudwnsth@gmail.com>
2025-11-18 16:07:53 +0900

    Remove /src and add initial server structure(server.cpp, game.hpp, game.cpp, protocol.hpp)

---
+++ server/protocol.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+// 오타 방지및 가독성 향상을 위한 매크로를 미리 정의.
+#define MSG_MOVE "MOVE"
+#define MSG_VALID "VALID"
+#define MSG_INVALID "INVALID"
+#define MSG_WIN "WIN"
+#define MSG_LOSE "LOSE"
+#define MSG_DRAW "DRAW"
+#define MSG_YOUR_TURN "YOUR_TURN"
+#define MSG_WAIT "WAIT"
+#define MSG_OPPENENT_MOVED "OPPENENT_MOVED"
+
